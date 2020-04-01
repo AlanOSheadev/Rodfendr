75392fb72e75ad256eccdbbcb66b086053d94780
Alan O Shea <alanosheaclash@gmail.com>
2020-04-01 14:12:28 +0000

    added link to products and localhost to allowed hosts

--- rodfendr/settings.py
+++ rodfendr/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('https://github.com/AlanOSheadev/Rodfendr'), 'localhost', 'rodfendr.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('https://github.com/AlanOSheadev/Rodfendr'),
+                 'rodfendr.herokuapp.com', 'localhost']
 
 
 # Application definition
